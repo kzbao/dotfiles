9e4d7792d7cdcd4029486c1fe01da98d01ec7d52
Kevin Bao <kevin@karat.io>
2017-09-07 02:51:27 -0700

    Updated emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
+(find-file "~/Dropbox/Main/todo.org")
 (setq inhibit-splash-screen t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -17,9 +18,6 @@
 (setq show-paren-delay 0)
 (winner-mode t)
 
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-
 (package-initialize)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
@@ -31,12 +29,13 @@
 
 (require 'helm)
 (require 'helm-config)
+(helm-mode 1)
 
 ;; Taken from http://tuhdo.github.io/helm-intro.html
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
@@ -44,13 +43,32 @@
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 (setq helm-split-window-in-side-p t ; open helm buffer inside current window
-      helm-ff-file-name-history-use-recentf t)
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-inheader-line t)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x h o") 'helm-occur)
 (global-set-key (kbd "C-x C-b") 'helm-mini)
+(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c h o") 'helm-occur)
 
-(helm-mode 1)
+(global-set-key (kbd "C-x C-a") 'mark-whole-buffer)
+(global-set-key (kbd "C-x C-k") 'kill-buffer)
+(global-unset-key (kbd "C-x k"))
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode evil-magit yasnippet solarized-theme rainbow-mode magit helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
