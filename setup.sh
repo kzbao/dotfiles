a523668809541ea4341a6da7d260c50b3830869e
Kevin Bao <kzbao93@gmail.com>
2018-04-19 16:09:13 -0700

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/zsh
+
+ln -s "$PWD/.emacs.d/init.el" ~/.emacs.d/init.el
+for file in .emacs.d/lisp/*; do
+    ln -s "$PWD/$file" ~/.emacs.d/lisp/
+done
+
+ln -s "$PWD/.gitconfig" ~/.gitconfig
+ln -s "$PWD/.zshrc" ~/.zshrc
