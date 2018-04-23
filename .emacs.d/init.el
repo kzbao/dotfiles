e31312a32c8902493a4d2f9d7bb023145d4ad534
Kevin Bao <kzbao93@gmail.com>
2018-04-23 13:45:40 -0700

    Changed default emacs font to Source Code Pro

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,21 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (load-library "options")
-(load-library "aliases")
 (load-library "packages")
+(load-library "aliases")
 
 ;; Open org file
 (find-file "~/Dropbox/Main/todo.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro")))))
