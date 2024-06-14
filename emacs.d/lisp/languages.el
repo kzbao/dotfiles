885915eaa7cd31fd09c59cdf626827bacf7bcfd9
Kevin Bao <kzb@kevinbao.com>
2024-06-14 12:29:38 +0900

    Lilypond

--- emacs.d/lisp/languages.el
+++ emacs.d/lisp/languages.el
@@ -7,4 +7,9 @@
 ;; JS
 (setq js-indent-level 2)
 
+;; LilyPond
+(add-to-list 'load-path "/opt/homebrew/Cellar/lilypond/2.24.3/share/emacs/site-lisp/lilypond")
+(autoload 'LilyPond-mode "lilypond-mode" "LilyPond editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.ly\\'" . LilyPond-mode))
+
 (provide 'languages)
